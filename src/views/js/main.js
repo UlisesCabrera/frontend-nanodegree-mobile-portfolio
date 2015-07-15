81abb61972051dc9123b47a00031d81ca6a6a8c7
Ulises Cabrera <ucabrera@outlook.com>
2015-07-15 10:07:54 -0400

    Modified the pizza generator when the page loads

--- src/views/js/main.js
+++ src/views/js/main.js
@@ -549,15 +549,14 @@ window.addEventListener('scroll', function(argument) {
 document.addEventListener('DOMContentLoaded', function() {
     var cols = 8;
     var s = 256;
+    var movingPizzas = document.querySelector("#movingPizzas1");
     for (var i = 0; i < 200; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
         elem.src = "images/pizza.png";
-        elem.style.height = "100px";
-        elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
-        document.querySelector("#movingPizzas1").appendChild(elem);
+        movingPizzas.appendChild(elem);
     }
     requestAnimationFrame(updatePositions);
 });
